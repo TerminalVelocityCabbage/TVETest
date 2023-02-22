ec91d9cb69bc77161dda76add2c3249d0d944271
Brandon Davis <neusfear@gmail.com>
2023-02-22 15:24:07 -0600

    Add Client and Server base classes

---
+++ src/main/java/com/terminalvelocitycabbage/game/client/GameClient.java
@@ -0,0 +1,34 @@
+package com.terminalvelocitycabbage.game.client;
+
+import com.terminalvelocitycabbage.engine.client.ClientBase;
+
+public class GameClient extends ClientBase {
+
+    public GameClient(String namespace) {
+        super(namespace);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void tick() {
+
+    }
+}
---
+++ src/main/java/com/terminalvelocitycabbage/game/server/GameServer.java
@@ -0,0 +1,29 @@
+package com.terminalvelocitycabbage.game.server;
+
+import com.terminalvelocitycabbage.engine.server.ServerBase;
+
+public class GameServer extends ServerBase {
+
+    public GameServer(String namespace) {
+        super(namespace);
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Override
+    public void tick() {
+
+    }
+}
