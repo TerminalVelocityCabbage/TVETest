6a26ee2a172cbd72e2b2cf034d7a72f97a9bc549
Brandon Davis <neusfear@gmail.com>
2023-02-22 15:33:37 -0600

    Add testing lifecycle messages

--- src/main/java/com/terminalvelocitycabbage/game/client/GameClient.java
+++ src/main/java/com/terminalvelocitycabbage/game/client/GameClient.java
@@ -1,34 +1,38 @@
 package com.terminalvelocitycabbage.game.client;
 
 import com.terminalvelocitycabbage.engine.client.ClientBase;
+import com.terminalvelocitycabbage.engine.debug.Log;
 
 public class GameClient extends ClientBase {
+
+    public static final String ID = "game";
 
     public GameClient(String namespace) {
         super(namespace);
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        GameClient client = new GameClient(ID);
+        client.start();
     }
 
     @Override
     public void init() {
-
+        Log.info("Game Client Started");
     }
 
     @Override
     public void destroy() {
-
+        Log.info("Game Client Stopped");
     }
 
     @Override
     public void update() {
-
+        Log.info("Game Client Updated");
     }
 
     @Override
     public void tick() {
-
+        Log.info("Game Client Ticked");
     }
 }
--- src/main/java/com/terminalvelocitycabbage/game/server/GameServer.java
+++ src/main/java/com/terminalvelocitycabbage/game/server/GameServer.java
@@ -1,29 +1,33 @@
 package com.terminalvelocitycabbage.game.server;
 
+import com.terminalvelocitycabbage.engine.debug.Log;
 import com.terminalvelocitycabbage.engine.server.ServerBase;
 
 public class GameServer extends ServerBase {
+
+    public static final String ID = "game";
 
     public GameServer(String namespace) {
         super(namespace);
     }
 
     public static void main(String[] args) {
-
+        GameServer server = new GameServer(ID);
+        server.start();
     }
 
     @Override
     public void init() {
-
+        Log.info("Game Server Started");
     }
 
     @Override
     public void destroy() {
-
+        Log.info("Game Server Stopped");
     }
 
     @Override
     public void tick() {
-
+        Log.info("Game Server Ticked");
     }
 }
