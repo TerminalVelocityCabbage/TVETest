8a665920e777d50f816bf3f3cbb924ba205117eb
Brandon Davis <neusfear@gmail.com>
2023-03-01 17:17:53 -0600

    Split into distinctive modules to avoid bad dependencies

--- Client/src/main/java/com/terminalvelocitycabbage/game/client/GameRenderer.java
+++ Client/src/main/java/com/terminalvelocitycabbage/game/client/GameRenderer.java
@@ -2,8 +2,7 @@ package com.terminalvelocitycabbage.game.client;
 
 import com.terminalvelocitycabbage.engine.client.ClientBase;
 import com.terminalvelocitycabbage.engine.client.renderer.RendererBase;
-
-import static org.lwjgl.opengl.GL11.*;
+import org.lwjgl.opengl.GL11;
 
 public class GameRenderer extends RendererBase {
 
@@ -21,26 +20,26 @@ public class GameRenderer extends RendererBase {
         int width = ClientBase.getInstance().getWindow().getWidth();
         int height = ClientBase.getInstance().getWindow().getHeight();
 
-        glClear(GL_COLOR_BUFFER_BIT);
-        glViewport(0, 0, width, height);
+        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+        GL11.glViewport(0, 0, width, height);
 
         long thisTime = System.nanoTime();
         float elapsed = (lastTime - thisTime) / 1E9f;
         lastTime = thisTime;
 
         float aspect = (float) width / height;
-        glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
-        glOrtho(-1.0f * aspect, aspect, -1.0f, +1.0f, -1.0f, +1.0f);
+        GL11.glMatrixMode(GL11.GL_PROJECTION);
+        GL11.glLoadIdentity();
+        GL11.glOrtho(-1.0f * aspect, aspect, -1.0f, +1.0f, -1.0f, +1.0f);
 
-        glMatrixMode(GL_MODELVIEW);
-        glRotatef(elapsed * 10.0f, 0, 0, 1);
-        glBegin(GL_QUADS);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(+0.5f, -0.5f);
-        glVertex2f(+0.5f, +0.5f);
-        glVertex2f(-0.5f, +0.5f);
-        glEnd();
+        GL11.glMatrixMode(GL11.GL_MODELVIEW);
+        GL11.glRotatef(elapsed * 10.0f, 0, 0, 1);
+        GL11.glBegin(GL11.GL_QUADS);
+        GL11.glVertex2f(-0.5f, -0.5f);
+        GL11.glVertex2f(+0.5f, -0.5f);
+        GL11.glVertex2f(+0.5f, +0.5f);
+        GL11.glVertex2f(-0.5f, +0.5f);
+        GL11.glEnd();
     }
 
 }
