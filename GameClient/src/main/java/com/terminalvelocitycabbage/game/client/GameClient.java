6b369a67af1fb0f75bf0267043fc78e50b861dc8
Brandon Davis <neusfear@gmail.com>
2023-07-02 23:08:10 -0500

    update engine and add config test

--- GameClient/src/main/java/com/terminalvelocitycabbage/game/client/GameClient.java
+++ GameClient/src/main/java/com/terminalvelocitycabbage/game/client/GameClient.java
@@ -1,10 +1,16 @@
 package com.terminalvelocitycabbage.game.client;
 
+import com.electronwill.nightconfig.core.CommentedConfig;
+import com.electronwill.nightconfig.core.ConfigFormat;
+import com.electronwill.nightconfig.core.file.FileConfig;
+import com.electronwill.nightconfig.core.io.ConfigWriter;
+import com.electronwill.nightconfig.toml.TomlFormat;
 import com.terminalvelocitycabbage.engine.client.ClientBase;
+import com.terminalvelocitycabbage.engine.config.TVConfig;
 import com.terminalvelocitycabbage.engine.debug.Log;
-import com.terminalvelocitycabbage.engine.filesystem.ResourceSource;
-import com.terminalvelocitycabbage.engine.filesystem.ResourceType;
 import com.terminalvelocitycabbage.engine.filesystem.resources.Resource;
+import com.terminalvelocitycabbage.engine.filesystem.resources.ResourceSource;
+import com.terminalvelocitycabbage.engine.filesystem.resources.ResourceType;
 import com.terminalvelocitycabbage.engine.filesystem.sources.MainSource;
 import com.terminalvelocitycabbage.engine.registry.Identifier;
 import com.terminalvelocitycabbage.game.common.StopServerPacket;
@@ -60,20 +66,34 @@ public class GameClient extends ClientBase {
 
         //Test
         testFileSystemRegistryStuff();
+        testNightConfig();
     }
 
+    //Test that this game has access to both itself and mods on its filesystem
+    // (it will never be accessed like this but is good to show it's possible for the future)
     public void testFileSystemRegistryStuff() {
 
         //List resources which are registered
         //getFileSystem().listResources();
 
-        //Test reading the string that is the config file
+        //Test reading the string that is the config file from this game
         Resource resource = ClientBase.getInstance().getFileSystem().getResource(ResourceType.DEFAULT_CONFIG, new Identifier("game", "test.toml"));
         Log.info(resource.asString());
 
-        //Test reading the string that is the mod config file
+        //Test reading the string that is the mod config file (this is the part that will never work like this)
         Resource modResource = ClientBase.getInstance().getFileSystem().getResource(ResourceType.DEFAULT_CONFIG, new Identifier("testmod", "testmod.toml"));
         Log.info(modResource.asString());
+    }
+
+    //TVE implements a few utilities on top of Night Config, we test those here
+    public void testNightConfig() {
+        //Test night config stuff
+        FileConfig config = TVConfig.getOrCreateFileConfig(ClientBase.getInstance().getFileSystem(), new Identifier("game", "test.toml"));
+        config.load();
+        ConfigFormat<CommentedConfig> tomlFormat = TomlFormat.instance();
+        ConfigWriter tomlWriter = tomlFormat.createWriter();
+        String toml = tomlWriter.writeToString(config);
+        Log.info(toml);
     }
 
     @Override
