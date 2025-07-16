ec3ce8bc50be4bc43b9b22bfceac54f7918f0af2
Brandon Davis <neusfear@gmail.com>
2025-07-15 21:54:45 -0500

    Update renderer example to show a (useless) route for conditional graphs
    & update engine

--- GameClient/src/main/java/com/terminalvelocitycabbage/game/client/registry/GameRenderers.java
+++ GameClient/src/main/java/com/terminalvelocitycabbage/game/client/registry/GameRenderers.java
@@ -15,10 +15,13 @@ public class GameRenderers {
 
     public static void init(RendererRegistrationEvent event) {
         DEFAULT_RENDER_GRAPH = event.register(new Identifier(GameClient.ID, "drawScene"),
-                RenderGraph.builder()
-                        .addRoutineNode(new Identifier(GameClient.ID, "updateRotations"), GameRoutines.DEFAULT_ROUTINE)
-                        .addRenderNode(DRAW_SCENE_RENDER_NODE, DrawSceneRenderNode.class, GameShaders.MESH_SHADER_PROGRAM_CONFIG)
-                        .build()
+                new RenderGraph(RenderGraph.RenderPath.builder()
+                        .route(new Identifier(GameClient.ID, "booleanRoute"), (capabilities) -> true,
+                                RenderGraph.RenderPath.builder()
+                                        .addRoutineNode(new Identifier(GameClient.ID, "updateRotations"), GameRoutines.DEFAULT_ROUTINE),
+                                RenderGraph.RenderPath.builder()
+                        ).addRenderNode(DRAW_SCENE_RENDER_NODE, DrawSceneRenderNode.class, GameShaders.MESH_SHADER_PROGRAM_CONFIG)
+                )
         ).getIdentifier();
     }
 
