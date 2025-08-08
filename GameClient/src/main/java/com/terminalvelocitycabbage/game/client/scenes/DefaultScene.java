f807f7cf23623ff9d975490760a2879edde4636b
Brandon Davis <neusfear@gmail.com>
2025-08-08 15:42:35 -0500

    Refactor scene from latest tve

--- GameClient/src/main/java/com/terminalvelocitycabbage/game/client/scenes/DefaultScene.java
+++ GameClient/src/main/java/com/terminalvelocitycabbage/game/client/scenes/DefaultScene.java
@@ -7,12 +7,12 @@ import com.terminalvelocitycabbage.engine.client.renderer.model.Mesh;
 import com.terminalvelocitycabbage.engine.client.renderer.model.MeshCache;
 import com.terminalvelocitycabbage.engine.client.renderer.model.Model;
 import com.terminalvelocitycabbage.engine.client.scene.Scene;
+import com.terminalvelocitycabbage.engine.debug.Log;
 import com.terminalvelocitycabbage.engine.ecs.Entity;
 import com.terminalvelocitycabbage.engine.ecs.Manager;
 import com.terminalvelocitycabbage.engine.filesystem.resources.ResourceCategory;
 import com.terminalvelocitycabbage.engine.graph.Routine;
 import com.terminalvelocitycabbage.engine.registry.Identifier;
-import com.terminalvelocitycabbage.engine.registry.Registry;
 import com.terminalvelocitycabbage.game.client.GameClient;
 import com.terminalvelocitycabbage.game.client.registry.GameModels;
 import com.terminalvelocitycabbage.game.common.ecs.components.PitchYawRotationComponent;
@@ -20,23 +20,15 @@ import com.terminalvelocitycabbage.game.common.ecs.components.PlayerCameraCompon
 import com.terminalvelocitycabbage.game.common.ecs.components.PositionComponent;
 import com.terminalvelocitycabbage.templates.ecs.components.ModelComponent;
 import com.terminalvelocitycabbage.templates.ecs.components.TransformationComponent;
-import com.terminalvelocitycabbage.templates.events.MeshRegistrationEvent;
-import com.terminalvelocitycabbage.templates.events.ModelConfigRegistrationEvent;
 
 import java.util.List;
 
 public class DefaultScene extends Scene {
 
     private Entity playerEntity;
-    private Registry<Mesh> meshRegistry;
-    private MeshCache meshCache;
-    private Registry<Model> modelRegistry;
 
     public DefaultScene(Identifier renderGraph, Routine... routines) {
         super(renderGraph, List.of(routines));
-
-        this.meshRegistry = new Registry<>();
-        this.modelRegistry = new Registry<>();
     }
 
     @Override
@@ -44,14 +36,6 @@ public class DefaultScene extends Scene {
         GameClient client = (GameClient) ClientBase.getInstance();
         Manager manager = client.getManager();
 
-        client.getEventDispatcher().dispatchEvent(new MeshRegistrationEvent(meshRegistry));
-        client.getEventDispatcher().dispatchEvent(new ModelConfigRegistrationEvent(modelRegistry));
-
-        //Generate mesh cache
-        meshCache = new MeshCache(modelRegistry, meshRegistry, getTextureCache());
-
-        //TODO replace with registry for meshes. mesh should include an identifier to a texture
-        //TODO replace component Mesh field with an identifier for that mesh
         var entity = manager.createEntity();
         entity.addComponent(ModelComponent.class).setModel(GameModels.SMILE_SQUARE_MODEL);
         entity.addComponent(TransformationComponent.class).setPosition(-1.25f, 0, -2);
@@ -84,17 +68,13 @@ public class DefaultScene extends Scene {
     }
 
     @Override
+    public MeshCache createMeshCache() {
+        var client = ClientBase.getInstance();
+        return new MeshCache(client.getModelRegistry(), client.getMeshRegistry(), getTextureCache());
+    }
+
+    @Override
     public void cleanup() {
         getTextureCache().cleanup();
-    }
-
-    @Override
-    public Mesh getMesh(Identifier model) {
-        return meshCache.getMesh(model);
-    }
-
-    @Override
-    public Texture getTexture(Identifier model) {
-        return getTextureCache().getTexture(modelRegistry.get(model).getTextureIdentifier());
     }
 }
