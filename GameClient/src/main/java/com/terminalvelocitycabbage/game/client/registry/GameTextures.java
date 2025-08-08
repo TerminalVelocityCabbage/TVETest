e93c9534b0dd6c6b80ec704708df619d984488c2
Brandon Davis <neusfear@gmail.com>
2025-08-07 21:08:56 -0500

    Update tests to match the texture-atlases branch on TVE2

--- GameClient/src/main/java/com/terminalvelocitycabbage/game/client/registry/GameTextures.java
+++ GameClient/src/main/java/com/terminalvelocitycabbage/game/client/registry/GameTextures.java
@@ -8,10 +8,20 @@ import com.terminalvelocitycabbage.templates.events.ResourceRegistrationEvent;
 public class GameTextures {
 
     public static Identifier SMILE;
+    public static Identifier SAD;
+    public static Identifier SMILE_SMALL;
+    public static Identifier SMILE_TINY;
+    public static Identifier SAD_SMALL;
+    public static Identifier HAPPY;
 
     public static void init(ResourceRegistrationEvent event) {
         //Register Textures to the VFS
         SMILE = event.registerResource(GameClient.CLIENT_RESOURCE_SOURCE, ResourceCategory.TEXTURE, "smile.png").getIdentifier();
+        SAD = event.registerResource(GameClient.CLIENT_RESOURCE_SOURCE, ResourceCategory.TEXTURE, "sad.png").getIdentifier();
+        SMILE_SMALL = event.registerResource(GameClient.CLIENT_RESOURCE_SOURCE, ResourceCategory.TEXTURE, "smile_small.png").getIdentifier();
+        SAD_SMALL = event.registerResource(GameClient.CLIENT_RESOURCE_SOURCE, ResourceCategory.TEXTURE, "sad_small.png").getIdentifier();
+        SMILE_TINY = event.registerResource(GameClient.CLIENT_RESOURCE_SOURCE, ResourceCategory.TEXTURE, "smile_tiny.png").getIdentifier();
+        HAPPY = event.registerResource(GameClient.CLIENT_RESOURCE_SOURCE, ResourceCategory.TEXTURE, "happy.png").getIdentifier();
     }
 
 }
--- GameClient/src/main/java/com/terminalvelocitycabbage/game/client/scenes/DefaultScene.java
+++ GameClient/src/main/java/com/terminalvelocitycabbage/game/client/scenes/DefaultScene.java
@@ -1,6 +1,9 @@
 package com.terminalvelocitycabbage.game.client.scenes;
 
 import com.terminalvelocitycabbage.engine.client.ClientBase;
+import com.terminalvelocitycabbage.engine.client.renderer.materials.TextureCache;
+import com.terminalvelocitycabbage.engine.client.renderer.model.Mesh;
+import com.terminalvelocitycabbage.engine.client.renderer.model.Vertex;
 import com.terminalvelocitycabbage.engine.client.scene.Scene;
 import com.terminalvelocitycabbage.engine.ecs.Entity;
 import com.terminalvelocitycabbage.engine.ecs.Manager;
@@ -8,13 +11,42 @@ import com.terminalvelocitycabbage.engine.filesystem.resources.ResourceCategory;
 import com.terminalvelocitycabbage.engine.graph.Routine;
 import com.terminalvelocitycabbage.engine.registry.Identifier;
 import com.terminalvelocitycabbage.game.client.GameClient;
+import com.terminalvelocitycabbage.game.common.ecs.components.PitchYawRotationComponent;
+import com.terminalvelocitycabbage.game.common.ecs.components.PlayerCameraComponent;
+import com.terminalvelocitycabbage.game.common.ecs.components.PositionComponent;
+import com.terminalvelocitycabbage.templates.ecs.components.MaterialComponent;
+import com.terminalvelocitycabbage.templates.ecs.components.MeshComponent;
 import com.terminalvelocitycabbage.templates.ecs.components.TransformationComponent;
 
 import java.util.List;
 
+import static com.terminalvelocitycabbage.game.client.data.MeshData.MESH_FORMAT;
+import static com.terminalvelocitycabbage.game.client.registry.GameTextures.SMILE;
+
 public class DefaultScene extends Scene {
 
     private Entity playerEntity;
+
+    //TODO replace these with a model/mesh registered to a model/mesh cache so we can sort the meshes needing to be rendered by type and reduce the number of mesh binds
+    private static final Vertex[] TEST_VERTICES = new Vertex[] {
+            new Vertex(MESH_FORMAT)
+                    .setXYZPosition(-1f, 1f, 0f)
+                    .setRGBColor(0.5f, 0.0f, 0.0f)
+                    .setUV(0, 0),
+            new Vertex(MESH_FORMAT)
+                    .setXYZPosition(-1f, -1f, 0f)
+                    .setRGBColor(0.0f, 0.5f, 0.0f)
+                    .setUV(0, 1),
+            new Vertex(MESH_FORMAT)
+                    .setXYZPosition(1f, -1f, 0f)
+                    .setRGBColor(0.0f, 0.0f, 0.5f)
+                    .setUV(1, 1),
+            new Vertex(MESH_FORMAT)
+                    .setXYZPosition(1f, 1f, 0f)
+                    .setRGBColor(0.0f, 0.5f, 0.5f)
+                    .setUV(1, 0)
+    };
+    private static final int[] TEST_INDEXES = new int[] {0, 1, 3, 3, 1, 2};
 
     public DefaultScene(Identifier renderGraph, Routine... routines) {
         super(renderGraph, List.of(routines));
@@ -25,14 +57,38 @@ public class DefaultScene extends Scene {
         GameClient client = (GameClient) ClientBase.getInstance();
         Manager manager = client.getManager();
 
-        client.getFileSystem().getResourcesOfType(ResourceCategory.TEXTURE).forEach((key, value) -> getTextureCache().createTexture(key, value));
+        //TODO replace with registry for meshes. mesh should include an identifier to a texture
+        //TODO replace component Mesh field with an identifier for that mesh
+        var entity = manager.createEntity();
+        entity.addComponent(MeshComponent.class).setMesh(new Mesh(MESH_FORMAT, TEST_VERTICES, TEST_INDEXES));
+        entity.addComponent(MaterialComponent.class).setTexture(SMILE);
+        entity.addComponent(TransformationComponent.class).setPosition(0, 0, -2);
 
         playerEntity = manager.createEntity();
         playerEntity.addComponent(TransformationComponent.class);
+        playerEntity.addComponent(PositionComponent.class);
+        playerEntity.addComponent(PitchYawRotationComponent.class);
+        playerEntity.addComponent(PlayerCameraComponent.class);
+    }
+
+    @Override
+    public TextureCache createTextureCache() {
+
+        Identifier entityAtlas = ClientBase.getInstance().identifierOf("entityAtlas");
+
+        var builder = TextureCache.builder();
+        builder.addAtlas(entityAtlas);
+        //Add all textures to this atlas
+        //TODO add a way to flag resources on registration or add some utility to just addTexture from an identifier instead of needing to specify the id and resource
+        ClientBase.getInstance().getFileSystem().getResourcesOfType(ResourceCategory.TEXTURE).forEach((key, value) -> {
+            builder.addTexture(key, value, entityAtlas);
+        });
+
+        return builder.build();
     }
 
     @Override
     public void cleanup() {
-
+        getTextureCache().cleanup();
     }
 }
