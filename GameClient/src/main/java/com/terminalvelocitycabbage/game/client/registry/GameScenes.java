a1a2fb28b081672e07e8786dee4174d5bffde1aa
Brandon Davis <neusfear@gmail.com>
2026-03-07 20:17:36 -0600

    Replace default scene class with builder registration

--- GameClient/src/main/java/com/terminalvelocitycabbage/game/client/registry/GameScenes.java
+++ GameClient/src/main/java/com/terminalvelocitycabbage/game/client/registry/GameScenes.java
@@ -1,8 +1,8 @@
 package com.terminalvelocitycabbage.game.client.registry;
 
+import com.terminalvelocitycabbage.engine.client.scene.Scene;
 import com.terminalvelocitycabbage.engine.registry.Identifier;
 import com.terminalvelocitycabbage.game.client.GameClient;
-import com.terminalvelocitycabbage.game.client.scenes.DefaultScene;
 import com.terminalvelocitycabbage.templates.events.SceneRegistrationEvent;
 
 public class GameScenes {
@@ -10,7 +10,17 @@ public class GameScenes {
     public static Identifier DEFAULT_SCENE;
 
     public static void init(SceneRegistrationEvent event) {
-        DEFAULT_SCENE = event.registerScene(GameClient.ID, "default", new DefaultScene(GameRenderers.DEFAULT_RENDER_GRAPH, GameRoutines.DEFAULT_ROUTINE));
+        DEFAULT_SCENE = event.registerScene(GameClient.ID, "default", Scene.builder()
+                .renderGraph(GameRenderers.DEFAULT_RENDER_GRAPH)
+                .routines(GameRoutines.DEFAULT_ROUTINE)
+                .inputControllers(GameInput.UI_CLICK, GameInput.RELOAD_SHADERS, GameInput.PAUSE_SPINNING, GameInput.MOVE_AROUND, GameInput.LOOK_AROUND, GameInput.SCROLL)
+                .textureAtlases(GameTextures.DEFAULT_SCENE_ATLAS)
+                .entities(manager -> {
+                    manager.createEntityFromTemplate(GameEntities.SMILE_SQUARE_ENTITY);
+                    manager.createEntityFromTemplate(GameEntities.SAD_SQUARE_ENTITY);
+                    manager.createEntityFromTemplate(GameEntities.PLAYER_ENTITY);
+                })
+                .build());
     }
 
 }
