e43e71443c7ea62c089e10cc7050b241eccc8449
Brandon Davis <neusfear@gmail.com>
2026-03-09 20:26:42 -0500

    refactor rotate system

--- GameClient/src/main/java/com/terminalvelocitycabbage/game/client/ecs/RotateEntitiesSystem.java
+++ GameClient/src/main/java/com/terminalvelocitycabbage/game/client/ecs/RotateEntitiesSystem.java
@@ -1,15 +1,21 @@
 package com.terminalvelocitycabbage.game.client.ecs;
 
+import com.terminalvelocitycabbage.engine.client.ClientBase;
+import com.terminalvelocitycabbage.engine.client.scene.Scene;
 import com.terminalvelocitycabbage.engine.ecs.Manager;
 import com.terminalvelocitycabbage.engine.ecs.System;
+import com.terminalvelocitycabbage.game.client.registry.GameStates;
 import com.terminalvelocitycabbage.templates.ecs.components.TransformationComponent;
 
 public class RotateEntitiesSystem extends System {
 
     @Override
-    public void update(Manager manager, float deltaTime) {
-        manager.getEntitiesWith(TransformationComponent.class).forEach(entity -> {
-            entity.getComponent(TransformationComponent.class).rotate(0, 1f, 0);
-        });
+    public void update(Manager manager, float deltaTime, Scene scene) {
+        var stateHandler = scene != null ? scene.getStateHandler() : ClientBase.getInstance().getStateHandler();
+        if (stateHandler.<Boolean>getState(GameStates.ROTATE_ENTITIES).getValue()) {
+            manager.getEntitiesWith(TransformationComponent.class).forEach(entity -> {
+                entity.getComponent(TransformationComponent.class).rotate(0, 1f, 0);
+            });
+        }
     }
 }
