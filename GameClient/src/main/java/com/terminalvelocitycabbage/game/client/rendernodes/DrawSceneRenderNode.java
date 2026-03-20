2c4ab3cacfef752053766fa27319bacdbe750d77
Brandon Davis <neusfear@gmail.com>
2026-03-20 14:49:11 -0500

    Optimize renderer for same models/textures

--- GameClient/src/main/java/com/terminalvelocitycabbage/game/client/rendernodes/DrawSceneRenderNode.java
+++ GameClient/src/main/java/com/terminalvelocitycabbage/game/client/rendernodes/DrawSceneRenderNode.java
@@ -1,11 +1,13 @@
 package com.terminalvelocitycabbage.game.client.rendernodes;
 
+import com.terminalvelocitycabbage.engine.client.renderer.model.Model;
 import com.terminalvelocitycabbage.engine.client.renderer.shader.ShaderProgramConfig;
 import com.terminalvelocitycabbage.engine.client.scene.Scene;
 import com.terminalvelocitycabbage.engine.client.window.WindowProperties;
 import com.terminalvelocitycabbage.engine.debug.Log;
 import com.terminalvelocitycabbage.engine.ecs.Entity;
 import com.terminalvelocitycabbage.engine.graph.RenderNode;
+import com.terminalvelocitycabbage.engine.registry.Identifier;
 import com.terminalvelocitycabbage.engine.util.HeterogeneousMap;
 import com.terminalvelocitycabbage.game.client.GameClient;
 import com.terminalvelocitycabbage.game.client.registry.GameRenderers;
@@ -43,20 +45,41 @@ public class DrawSceneRenderNode extends RenderNode {
         //Sort entities for efficient rendering (by texture then by model)
         List<Entity> entities = new ArrayList<>(client.getManager().getEntitiesWith(ModelComponent.class, TransformationComponent.class));
         entities.sort(Comparator
-                        .comparingInt((Entity entity) -> client.getTextureCache().getTexture(client.getModelRegistry().get(entity.getComponent(ModelComponent.class).getModel()).atlasIdentifier()).getTextureID())
+                        .comparingInt((Entity entity) -> client.getTextureCache().getTexture(client.getModelRegistry().get(entity.getComponent(ModelComponent.class).getModel()).textureIdentifier()).getTextureID())
                         .thenComparing(entity -> entity.getComponent(ModelComponent.class).getModel().hashCode())
         );
 
         //Render entities
+        Identifier lastTextureID = null;
+        Identifier lastModelID = null;
+        Model model;
         for (Entity entity : entities) {
-            var modelIdentifier = entity.getComponent(ModelComponent.class).getModel();
-            var model = client.getModelRegistry().get(modelIdentifier);
-            var transformationComponent = entity.getComponent(TransformationComponent.class);
 
-            shaderProgram.getUniform("modelMatrix").setUniform(transformationComponent.getTransformationMatrix());
+            //Update the transformation to that of this entity
+            shaderProgram.getUniform("modelMatrix").setUniform(entity.getComponent(TransformationComponent.class).getTransformationMatrix());
+
+            //Early draw if this is the same model as the last entity (save on uploads)
+            var modelIdentifier = entity.getComponent(ModelComponent.class).getModel();
+            if (modelIdentifier.equals(lastModelID)) {
+                client.getModelRegistry().get(modelIdentifier).draw();
+                continue;
+            } else {
+                lastModelID = modelIdentifier;
+                model = client.getModelRegistry().get(modelIdentifier);
+            }
+
             if (model.compiledMesh().getFormat().equals(shaderProgram.getConfig().getVertexFormat())) {
-                //TODO figure out a way to not need to bind the model and texture every frame if they're the same as last frame
-                model.render(client.getTextureCache());
+                //Optimization: only bind texture and mesh if they've changed since the last entity
+                var textureIdentifier = model.textureIdentifier();
+                if (!textureIdentifier.equals(lastTextureID)) {
+                    model.bindTexture(client.getTextureCache());
+                    lastTextureID = textureIdentifier;
+                }
+
+                model.bind();
+                model.draw();
+            } else {
+                Log.error("Model " + modelIdentifier + " has a different vertex format than the shader program!");
             }
         }
 
